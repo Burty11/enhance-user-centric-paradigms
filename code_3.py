f01ab8b78cf7353ebd38de0b5d064047c41d2ac4
Burty11 <142454463+Burty11@users.noreply.github.com>
2024-03-05 20:55:54 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,42 @@
+
+import random
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    main()
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+
+    return data
+def generate_random_data():
+    for item in data:}
+    const data = generateRandomData();
+    return data;
+main();
+
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    });
+function generateRandomData() {
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+
+    data.forEach(item => {
+    }
+
+function main() {        }
+    public static void main(String[] args) {
+}
+    }
+            System.out.println("Random Number: " + item);
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+        }
+        for (int item : data) {
+public class RandomDataGenerator {
+
+        int[] data = new int[10];
